117a2020f6d31a522b8aefe529c08d55738de755
Alexs Ruiz <ralexs.acu@gmail.com>
2023-05-30 22:57:57 -0600

    use gif card instead of Text widget

--- lib/screens/giphy_screen.dart
+++ lib/screens/giphy_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:giphy_app/widgets/gif_card.dart';
 
 class GiphyScreen extends StatelessWidget {
   const GiphyScreen({super.key});
@@ -21,7 +22,12 @@ class _GiphyView extends StatelessWidget {
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 10),
         child: Column(
-          children: const [Text('This is a test')],
+          children: const [
+            GifCard(
+              imageUrl:
+                  'https://media0.giphy.com/media/vAGq7eYHmYJu3s9zUS/giphy.gif?cid=b6b005e2b1ri23zusoym3hkz77chrsgz0nh2ra98a2qs6peo&ep=v1_gifs_trending&rid=giphy.gif&ct=g',
+            )
+          ],
         ),
       ),
     );

31636de1e659426c40423d396b6fa453a6b62ebc
Alexs Ruiz <ralexs.acu@gmail.com>
2023-05-30 23:38:43 -0600

    remove Column and use the new GifLayout Widget

--- lib/screens/giphy_screen.dart
+++ lib/screens/giphy_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:giphy_app/widgets/gif_card.dart';
+import 'package:giphy_app/widgets/gif_layout.dart';
 
 class GiphyScreen extends StatelessWidget {
   const GiphyScreen({super.key});
@@ -18,17 +18,10 @@ class GiphyScreen extends StatelessWidget {
 class _GiphyView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
+    return const SafeArea(
       child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 10),
-        child: Column(
-          children: const [
-            GifCard(
-              imageUrl:
-                  'https://media0.giphy.com/media/vAGq7eYHmYJu3s9zUS/giphy.gif?cid=b6b005e2b1ri23zusoym3hkz77chrsgz0nh2ra98a2qs6peo&ep=v1_gifs_trending&rid=giphy.gif&ct=g',
-            )
-          ],
-        ),
+        padding: EdgeInsets.symmetric(horizontal: 10),
+        child: GifLayout(),
       ),
     );
   }
